d6dc4c637f1751dab8269f3b13bf246242cef693
YoannGIORDANOG22013541 <115475641+GIORDANO-yoann-2225038a@users.noreply.github.com>
2025-02-03 17:27:42 +0100

    regles erreur

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupérer l'erreur d'authentification (si elle existe)
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Récupérer le dernier username utilisé
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+        // Le contrôle de la déconnexion est effectué automatiquement
+        // Par défaut, Symfony gère la déconnexion automatiquement
+    }
+}
+
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user->setEmail('email@test.com'.$i);
+            $manager->persist($user);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
