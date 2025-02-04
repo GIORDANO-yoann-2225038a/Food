e54e5d67e297e43d5da48bfbdb89c9b3aa0866fb
YoannGIORDANOG22013541 <115475641+GIORDANO-yoann-2225038a@users.noreply.github.com>
2025-02-04 19:42:19 +0100

    ajout de la sécurité concernant les déplacement de page par l'url et add bouton déconnexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,14 +9,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]  // Bloque l'accès aux utilisateurs non connectés
     function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'message' => 'User successfully created!'
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['POST'])]
+    public function logout(): void
+    {
+        // Symfony gère la déconnexion automatiquement
+        throw new \Exception('This should never be reached!');
     }
 }
