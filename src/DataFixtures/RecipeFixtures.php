092ac340a5dd76ee2cea6640ff21b6e253d1a3a3
YoannGIORDANOG22013541 <115475641+GIORDANO-yoann-2225038a@users.noreply.github.com>
2025-02-09 16:58:13 +0100

    add appelle API pour récup plats et ajout d'image du styles du bouton reload

--- src/DataFixtures/RecipeFixtures.php
+++ src/DataFixtures/RecipeFixtures.php
@@ -12,17 +12,17 @@ class RecipeFixtures extends Fixture
     {
         $recipes = [
             'Quiche lorraine' => 'crème fraiche, lardons, oeufs, gruyère, pâte feuilletée',
-            'Quiche légumes' => 'moutarde, pâte brisée, aubergines, poivrons, fromage',
-            'Tarte tomate' => 'moutarde, tomates',
-            'Spaghettis carbo' => 'lardons, crème fraiche, spaghettis, parmesan',
-            'Spaghettis bolo' => 'viande de boeuf, sauce tomate, spaghettis, oignon',
+            'Tarte légumes' => 'moutarde, pâte brisée, aubergines, poivrons, fromage',
+            'Tarte à la tomate' => 'moutarde, tomates',
+            'Spaghettis carbonara' => 'lardons, crème fraiche, spaghettis, parmesan',
+            'Spaghettis bolognaise' => 'viande de boeuf, sauce tomate, spaghettis, oignon',
             'Gnocchi sauce tomate' => 'gnocchi, sauce tomate, oignons',
             'Poulet curry' => 'épices curry, poulet, lait de coco, oignons',
-            'Poulet pâtes à la crème' => 'poulet, pâtes, crème fraiche, gruyère, persil',
+            'Poulet pâtes' => 'poulet, pâtes, crème fraiche, gruyère, persil',
             'Poke Bowl' => 'saumon, avocat, riz vinaigré, concombre, graines de sésame',
             'Riz Poulet' => 'riz, poulet, haricots, lardons, sauce soja',
             'Salade' => 'salade, tomates, concombre, feta, olives, oignons, vinaigrette',
-            'gratin dauhinois' => 'pommes de terre, crème fraiche, ail, gruyère',
+            'Gratin dauphinois' => 'pommes de terre, crème fraiche, ail, gruyère',
             'Steak patate' => 'steak, patate, oignon, ail, persil',
             'Gnocchi de butternut' => 'butternut, farine, parmesan',
 
